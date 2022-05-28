6d83e2b4dcda505b0b0a291a72986a087a231d84
igor.abrashin <igor.abrashin@novatek.ru>
2022-05-28 11:43:51 +0500

    add openapi spec on /api

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Catch API')
+    .setDescription('The catch API description')
+    .setVersion('1.0')
+    .addTag('catch')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({whitelist: true}))
   await app.listen(8080);
 }
